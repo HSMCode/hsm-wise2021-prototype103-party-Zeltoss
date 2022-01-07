6ecce4470adf16e108161e2335665928cd84dac6
Zeltoss <92942461+Zeltoss@users.noreply.github.com>
2022-01-07 12:12:26 +0100

    Added Background Show/Hide Script for Countdown

---
+++ Assets/Scripts/CountdownBackground.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownBackground : MonoBehaviour
+{
+    float Background = 0f;
+    float pause = 2f;
+      
+      void Start()
+      {
+          gameObject.SetActive(false);
+          Invoke("Active", Background);
+      }
+ 
+      void Active()
+      {
+          gameObject.SetActive(true);
+          Invoke("Deactive", pause);
+      }
+
+      void Deactive()
+      {
+          gameObject.SetActive(false);
+      }
+}

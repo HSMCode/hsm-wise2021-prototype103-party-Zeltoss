41754d12614dc063cd0c2717907878ec20ba19cf
Zeltoss <92942461+Zeltoss@users.noreply.github.com>
2022-01-21 21:20:08 +0100

    Update V0.3.2
    - Fixed Scaling Error in WebGL
    - Added Mute Button in Main Menu (Beta)
    - Added new Font
    - Added Main Menu Game Title
    - Changed "How To Play" Text

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Image soundOnIcon;
+    [SerializeField] Image soundOffIcon;
+    private bool muted = false;
+
+    public void OnButtonPress()
+    {
+        if(muted == false)
+        {
+            muted = true;
+            AudioListener.pause = true;
+        }
+        else
+        {
+            muted = false;
+            AudioListener.pause = false;
+        }
+    }
+
+}
